c8ecfbe6776fce9d678fd1046e2161df618f5c47
auto200 <49625375+auto200@users.noreply.github.com>
2020-06-18 18:24:56 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dzienik</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+	<h1>Dziennik</h1>
+</div>
+<div class="lewy">
+	<div>
+		<h3>Szukaj ucznia</h3>
+		<form>
+			Imie: <input type="text"><br>
+			<button type="submit">Szukaj</button>
+		</form>
+	</div>
+	<hr>
+	<div>
+		<h3>Dodaj ucznia</h3>
+		<form>
+			Imie: <input type="text" name="imie"><br>
+			Nazwisko: <input type="text" name="nazwisko"><br>
+			Wiek: <input type="number" name="wiek"><br>
+			<button type="submit">Dodaj</button>
+		</form>
+	</div>
+</div>
+<div class="prawy">
+	<h1>Lista Uczniów</h1>
+</div>
+<div class="stopka">Subskrybuj</div>
+</body>
+</html>
